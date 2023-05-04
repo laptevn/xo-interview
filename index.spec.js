2e2dd2f424d68e75c7f368e2d73ef1e5436a1e5b
Nickolay Laptev <nickolay.laptev@gmail.com>
2023-05-04 19:43:41 +0400

    Add concurrent test

--- index.spec.js
+++ index.spec.js
@@ -27,5 +27,36 @@ describe('API tests', () => {
             remainingBalance: 0
         });
     }, 20000)
+
+    it('Concurrent requests', async () => {
+        var response = await request(API_URL).post(RESET_ENDPOINT);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBe(null);
+
+        const charges = new Set();
+        for (let sequentialCounter = 0; sequentialCounter < 2; ++sequentialCounter) {
+            const promises = [];
+            for (let i = 0; i < 10; ++i) {
+                promises.push(new Promise(async (resolve) => {
+                    const response = await request(API_URL).post(CHARGE_ENDPOINT);
+                    expect(response.statusCode).toBe(200);
+                    charges.add(response.body.remainingBalance);
+                    resolve(true);
+                }))
+            }
+
+            await Promise.all(promises);
+        }
+
+        expect(charges.size).toBe(20);
+
+        response = await request(API_URL).post(CHARGE_ENDPOINT);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toStrictEqual({
+            charges: 0,
+            isAuthorized: false,
+            remainingBalance: 0
+        });
+    }, 10000)
 })
 
